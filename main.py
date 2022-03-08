6a47db299e7978a9a950dc9eff6821ec4526d99b
jackey10055206 <jackey10055206@gmail.com>
2022-03-08 19:13:41 +0800

    adding table client

--- main.py
+++ main.py
@@ -7,20 +7,32 @@ from PyQt5.QtGui import *
 from tkinter import *
 
 import project as ui
-# import pymysql
+import pymysql
 # import openpyxl
 
-
+connection = pymysql.connect(host='192.168.101.59',
+                             port=3306,
+                             user='root',
+                             passwd='root',
+                             database='work_order',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+#print(bool(connection))
+cursor = connection.cursor()
 class Main(QMainWindow, ui.Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-    # 查解析度
-    # root = Tk()
-    # monitor_height = root.winfo_screenheight()
-    # monitor_width = root.winfo_screenwidth()
-    # print("width x height = %d x %d (pixels)" %(monitor_width, monitor_height))
- 
+
+    SQL = """INSERT INTO client(name,full_name,phone,address,taxID)
+             VALUES('228基金會','財團法人二二八事件紀念基金會','23326228','10066台北市南海路54號','97971614'),
+             ('228共生','社團法人台灣共生青年協會','無','10351台北市大同區長安西路84號4樓之一','85202463')
+          """
+    
+    cursor.execute(SQL)
+    connection.commit()
+    
+
 
 
 if __name__ == '__main__':
