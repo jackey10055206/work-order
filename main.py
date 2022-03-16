30424f02225f128fd8ef4a734c1df5ae703674b9
jackey10055206 <jackey10055206@gmail.com>
2022-03-16 16:48:17 +0800

    working on cal_CBM

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import sys
 from this import s
 from PyQt5 import QtWidgets
@@ -38,120 +39,37 @@ class Main(QMainWindow, ui.Ui_Form):
             #print(full_name,phone,address,taxID)
             self.lineEdit_phone.setText(phone)
         self.comboBox_company_name.currentIndexChanged.connect(index_change)
+
         
-        self.comboBox_employee_1.addItems(employee_name())
-        self.comboBox_employee_2.addItems(employee_name())
-        self.comboBox_employee_3.addItems(employee_name())
-        self.comboBox_employee_4.addItems(employee_name())
-        self.comboBox_employee_5.addItems(employee_name())
+        for i in range(1,6):
+            exec("self.comboBox_employee_"+str(i)+".addItems(employee_name())")
                 
         self.comboBox_pack.addItems(pack_name())
         self.comboBox_transport.addItems(transport_name())
 
-        self.comboBox_product_1.addItems(product_name())
-        self.comboBox_product_2.addItems(product_name())
-        self.comboBox_product_3.addItems(product_name())
-        self.comboBox_product_4.addItems(product_name())
-        self.comboBox_product_5.addItems(product_name())
-        self.comboBox_product_6.addItems(product_name())
-        self.comboBox_product_7.addItems(product_name())
-        self.comboBox_product_8.addItems(product_name())
-        self.comboBox_product_9.addItems(product_name())
-        self.comboBox_product_10.addItems(product_name())
-        self.comboBox_product_11.addItems(product_name())
-        self.comboBox_product_12.addItems(product_name())
-        self.comboBox_product_13.addItems(product_name())
-        self.comboBox_product_14.addItems(product_name())
-        self.comboBox_product_15.addItems(product_name())
-
-        self.comboBox_material_1.addItems(material_name())
-        self.comboBox_material_2.addItems(material_name())
-        self.comboBox_material_3.addItems(material_name())
-        self.comboBox_material_4.addItems(material_name())
-        self.comboBox_material_5.addItems(material_name())
-        self.comboBox_material_6.addItems(material_name())
-        self.comboBox_material_7.addItems(material_name())
-        self.comboBox_material_8.addItems(material_name())
-        self.comboBox_material_9.addItems(material_name())
-        self.comboBox_material_10.addItems(material_name())
-        self.comboBox_material_11.addItems(material_name())
-        self.comboBox_material_12.addItems(material_name())
-        self.comboBox_material_13.addItems(material_name())
-        self.comboBox_material_14.addItems(material_name())
-        self.comboBox_material_15.addItems(material_name())
-
-        self.comboBox_process_1.addItems(process_name())
-        self.comboBox_process_2.addItems(process_name())
-        self.comboBox_process_3.addItems(process_name())
-        self.comboBox_process_4.addItems(process_name())
-        self.comboBox_process_5.addItems(process_name())
-        self.comboBox_process_6.addItems(process_name())
-        self.comboBox_process_7.addItems(process_name())
-        self.comboBox_process_8.addItems(process_name())
-        self.comboBox_process_9.addItems(process_name())
-        self.comboBox_process_10.addItems(process_name())
-        self.comboBox_process_11.addItems(process_name())
-        self.comboBox_process_12.addItems(process_name())
-        self.comboBox_process_13.addItems(process_name())
-        self.comboBox_process_14.addItems(process_name())
-        self.comboBox_process_15.addItems(process_name())
-
-        self.comboBox_plate_1.addItems(plate_name())
-        self.comboBox_plate_2.addItems(plate_name())
-        self.comboBox_plate_3.addItems(plate_name())
-        self.comboBox_plate_4.addItems(plate_name())
-        self.comboBox_plate_5.addItems(plate_name())
-        self.comboBox_plate_6.addItems(plate_name())
-        self.comboBox_plate_7.addItems(plate_name())
-        self.comboBox_plate_8.addItems(plate_name())
-        self.comboBox_plate_9.addItems(plate_name())
-        self.comboBox_plate_10.addItems(plate_name())
-        self.comboBox_plate_11.addItems(plate_name())
-        self.comboBox_plate_12.addItems(plate_name())
-        self.comboBox_plate_13.addItems(plate_name())
-        self.comboBox_plate_14.addItems(plate_name())
-        self.comboBox_plate_15.addItems(plate_name())
-
-        self.comboBox_thicknes_1.addItems(thickness_name())
-        self.comboBox_thicknes_2.addItems(thickness_name())
-        self.comboBox_thicknes_3.addItems(thickness_name())
-        self.comboBox_thicknes_4.addItems(thickness_name())
-        self.comboBox_thicknes_5.addItems(thickness_name())
-        self.comboBox_thicknes_6.addItems(thickness_name())
-        self.comboBox_thicknes_7.addItems(thickness_name())
-        self.comboBox_thicknes_8.addItems(thickness_name())
-        self.comboBox_thicknes_9.addItems(thickness_name())
-        self.comboBox_thicknes_10.addItems(thickness_name())
-        self.comboBox_thicknes_11.addItems(thickness_name())
-        self.comboBox_thicknes_12.addItems(thickness_name())
-        self.comboBox_thicknes_13.addItems(thickness_name())
-        self.comboBox_thicknes_14.addItems(thickness_name())
-        self.comboBox_thicknes_15.addItems(thickness_name())
-
-        self.comboBox_others_1.addItems(others_name())
-        self.comboBox_others_2.addItems(others_name())
-        self.comboBox_others_3.addItems(others_name())
-        self.comboBox_others_4.addItems(others_name())
-        self.comboBox_others_5.addItems(others_name())
-        self.comboBox_others_6.addItems(others_name())
-        self.comboBox_others_7.addItems(others_name())
-        self.comboBox_others_8.addItems(others_name())
-        self.comboBox_others_9.addItems(others_name())
-        self.comboBox_others_10.addItems(others_name())
-        self.comboBox_others_11.addItems(others_name())
-        self.comboBox_others_12.addItems(others_name())
-        self.comboBox_others_13.addItems(others_name())
-        self.comboBox_others_14.addItems(others_name())
-        self.comboBox_others_15.addItems(others_name())
-
-        def cal_CBM():
-            width = self.lineEdit_width_1.text()
-            height = self.lineEdit_height_1.text()
-            CBM=math.ceil(int(width)*int(height)/900)
-            self.lineEdit_CBM_1.setText(str(CBM))
+        for i in range(1,16):
+            exec("self.comboBox_product_"+str(i)+".addItems(product_name())")
+            exec("self.comboBox_material_"+str(i)+".addItems(material_name())")
+            exec("self.comboBox_process_"+str(i)+".addItems(process_name())")
+            exec("self.comboBox_plate_"+str(i)+".addItems(plate_name())")
+            exec("self.comboBox_thicknes_"+str(i)+".addItems(thickness_name())")
+            exec("self.comboBox_others_"+str(i)+".addItems(others_name())")
             
+        # def cal_CBM(i):
+        #     width = exec("self.lineEdit_width_"+str(i)+".text()")
+        #     height = exec("self.lineEdit_height_"+str(i)+".text()")
+        #     CBM=math.ceil(int(width)*int(height)/900)
+        #     exec("self.lineEdit_CBM_"+str(i)+".setText(str(CBM))")
+        #     exec("self.lineEdit_CBM_"+str(i)+".setText(CBM)")
+        
+        def cal_CBM(tmp):
+            print(tmp)
+        
+        for i in range(1,16):    
+            exec("self.lineEdit_height_"+str(i)+".editingFinished.connect(lambda:cal_CBM(i))")
             
-        self.lineEdit_height_1.editingFinished.connect(cal_CBM)
+        
+    
             
 
         
